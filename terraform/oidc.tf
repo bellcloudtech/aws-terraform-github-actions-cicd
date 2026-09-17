e2068ecd9b0e250b71e65ab4099fe6b1dd688fbe
Anthony Bell <bellcloudops@gmail.com>
2026-09-17 06:50:55 -0500

    Fix GitHub OIDC trust policy

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -30,10 +30,10 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     }
 
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values = [
-        "repo:bellcloudtech/aws-terraform-github-actions-cicd:ref:refs/heads/main"
+        "repo:bellcloudtech/aws-terraform-github-actions-cicd:*"
       ]
     }
   }
